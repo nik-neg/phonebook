64108cc49433f02d66b60ed60d01fa967ebc53a1
Alexander Genkin <alexander_genkin@gmx.net>
2023-03-04 19:16:14 +0200

    fix: shine

--- client/src/components/Dashboard/ContactsList/ContactsList.styles.ts
+++ client/src/components/Dashboard/ContactsList/ContactsList.styles.ts
@@ -24,12 +24,10 @@ export const SContactListContainerWrapper = styled.div`
     }
 `;
 export const SContactListContainer = styled.div`
-    padding-top: 0.25rem;
     display: flex;
     justify-content: center;
     width: 350px;
     border: 3px solid rgba(196, 196, 196, 0.67);
-    border-bottom: none;
     border-radius: 15px;
     height: 760px;
     background: url(${background}) repeat;
@@ -59,6 +57,76 @@ export const SContactListWrapper = styled.div<IContactListWrapper>`
 
     width: 100%;
     position: relative;
+
+    ${(props) =>
+        !props.contactsAreFetched &&
+        `
+        &:after {
+        content: '';
+        top: -10px;
+        transform: translateX(100%);
+        width: 225px;
+        height: 690px;
+        position: absolute;
+        z-index: 1;
+        animation: slide 6.67s infinite;
+        /*
+    CSS Gradient - complete browser support from http://www.colorzilla.com/gradient-editor/
+    */
+        background: -moz-linear-gradient(
+            left,
+            rgba(255, 255, 255, 0) 0%,
+            rgba(255, 255, 255, 0.8) 50%,
+            rgba(128, 186, 232, 0) 99%,
+            rgba(125, 185, 232, 0) 100%
+        ); /* FF3.6+ */
+        background: -webkit-gradient(
+            linear,
+            left top,
+            right top,
+            color-stop(0%, rgba(255, 255, 255, 0)),
+            color-stop(50%, rgba(255, 255, 255, 0.8)),
+            color-stop(99%, rgba(128, 186, 232, 0)),
+            color-stop(100%, rgba(125, 185, 232, 0))
+        ); /* Chrome,Safari4+ */
+        background: -webkit-linear-gradient(
+            left,
+            rgba(255, 255, 255, 0) 0%,
+            rgba(255, 255, 255, 0.8) 50%,
+            rgba(128, 186, 232, 0) 99%,
+            rgba(125, 185, 232, 0) 100%
+        ); /* Chrome10+,Safari5.1+ */
+        background: -o-linear-gradient(
+            left,
+            rgba(255, 255, 255, 0) 0%,
+            rgba(255, 255, 255, 0.8) 50%,
+            rgba(128, 186, 232, 0) 99%,
+            rgba(125, 185, 232, 0) 100%
+        ); /* Opera 11.10+ */
+        background: -ms-linear-gradient(
+            left,
+            rgba(255, 255, 255, 0) 0%,
+            rgba(255, 255, 255, 0.8) 50%,
+            rgba(128, 186, 232, 0) 99%,
+            rgba(125, 185, 232, 0) 100%
+        ); /* IE10+ */
+        background: linear-gradient(
+            to right,
+            rgba(255, 255, 255, 0) 0%,
+            rgba(255, 255, 255, 0.8) 50%,
+            rgba(128, 186, 232, 0) 99%,
+            rgba(125, 185, 232, 0) 100%
+        ); /* W3C */
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#007db9e8',GradientType=1 ); /* IE6-9 */
+    }
+    @keyframes slide {
+        0% {
+            transform: translateX(-100%);
+        }
+        100% {
+            transform: translateX(100%);
+        }
+    }`}
 `;
 
 export const SButtonPanelWrapper = styled.div`
