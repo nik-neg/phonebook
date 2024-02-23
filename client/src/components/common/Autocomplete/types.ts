938267a92cff96e53450ec151bd63a2edc98f554
Alexander Genkin <alexander_genkin@gmx.net>
2024-02-23 20:20:59 +0200

    setup: poc

--- client/src/components/common/Autocomplete/types.ts
+++ client/src/components/common/Autocomplete/types.ts
@@ -1,8 +1,14 @@
 import { IAddDialogState } from '../../Dashboard/dialogs/AddDialog';
 
+export interface IAutocompleteProps {
+    isSuggestionsVisible: boolean;
+    onHandleSuggestionsVisible: (value: boolean) => void;
+    [key: string]: any;
+}
 export interface AutoCompleteWrapperProps {
+    isSuggestionsVisible: boolean;
+    onHandleSuggestionsVisible: (value: boolean) => void;
     attributeName: string;
-    register: any;
     formFieldName: keyof IAddDialogState;
     handleSetValue: (name: keyof IAddDialogState, value: string) => void;
 }
